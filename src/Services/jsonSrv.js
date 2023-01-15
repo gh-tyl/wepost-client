997191a52b76e24689dcc5560e7551c92fbd4dfe
Tyler Inari <github@al.tyler.jp>
2023-01-14 23:10:12 -0800

    small fix to connect with api

--- src/Services/jsonSrv.js
+++ src/Services/jsonSrv.js
@@ -1,7 +1,6 @@
 import httpCommon from "../httpCommon";
 class jsonSrv {
     post(pageName, data) {
-        // data = JSON.stringify(data);
         return httpCommon.post(pageName, data);
     }
 }
