37ade3bffb6fb8bd3ad8dbd7a864d3bc85da1ed1
matheus santana <mgolem017@gmail.com>
2023-01-15 17:21:55 -0800

    Without paper design.

--- src/pages/admin/dashboard.js
+++ src/pages/admin/dashboard.js
@@ -1,25 +1,15 @@
-import CountryChart from "./charts/CountryChart";
-import TopicsChart from "./charts/TopicsChart";
-import AgeChart from "./charts/AgeChart"
-import GenderChart from "./charts/GenderChart"
-import Container from '@mui/material/Container';
+import CountryChart from "./components/charts/CountryChart";
+import TopicsChart from "./components/charts/TopicsChart";
+import AgeChart from "./components/charts/AgeChart"
+import GenderChart from "./components/charts/GenderChart"
 import Grid from "@mui/material/Grid";
-
-
+import Paper from '@mui/material/Paper';
 
 
 function Dashboard() {
 	return (
 		<>
-			<Grid container paddingY={4} textAlign={"center"}>
-				<Grid item sm={12}>
-					<Container maxWidth="xs">					  
-						<h1>Dashboard</h1>
-						<p>Dashboard page for admin</p>
-					</Container>
-				</Grid>
-			</Grid>
-			<Grid container paddingBottom={4} spacing={4} rowSpacing={6}>			
+			<Grid container spacing={4} rowSpacing={6} marginY={1}>			
 				<CountryChart />
 				<TopicsChart />
 				<AgeChart />
