4437a97195935893f27fb8518a9db00df80f9e39
Tyler Inari <github@al.tyler.jp>
2023-01-17 02:48:42 -0800

    update routing

--- src/Main.js
+++ src/Main.js
@@ -2,32 +2,36 @@ import { ReactDOM } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Layout from "./pages/Layout";
-import Home from "./pages/Home";
-import Feed from "./pages/articles/feed";
-import PostArticle from "./pages/articles/post";
-import Dashboard from "./pages/admin/dashboard";
 import Nopage from "./pages/Nopage";
-import Login from "./pages/auth/login";
-import Register from "./pages/auth/register";
-import Logout from "./pages/auth/logout";
-import Profile from "./pages/common/profile";
-import TestLogin from "./pages/test/TestLogin";
+import { pages, auths, tests } from './config/config';
 
 function Main() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<Layout />} >
-					<Route path="/" element={<Home />} />
-					<Route path="feed" element={<Feed />} />
-					<Route path="post_article" element={<PostArticle />} />
-					<Route path="dashboard" element={<Dashboard />} />
-					<Route path="login" element={<Login />} />
-					<Route path="register" element={<Register />} />
-					<Route path="logout" element={<Logout />} />
-					<Route path="profile" element={<Profile />} />
 					<Route path="*" element={<Nopage />} />
-					<Route path="test_login" element={<TestLogin />} />
+					{
+						pages.map((page, index) => {
+							return (
+								<Route key={index} path={page.path} element={<page.element />} />
+							)
+						})
+					}
+					{
+						auths.map((auth, index) => {
+							return (
+								<Route key={index} path={auth.path} element={<auth.element />} />
+							)
+						})
+					}
+					{
+						tests.map((test, index) => {
+							return (
+								<Route key={index} path={test.path} element={<test.element />} />
+							)
+						})
+					}
 				</Route>
 			</Routes>
 		</BrowserRouter>
