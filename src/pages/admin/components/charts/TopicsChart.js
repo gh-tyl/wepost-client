e6151c5ac1285550084175825ceeca334cc2e50f
matheus santana <mgolem017@gmail.com>
2023-01-18 00:07:21 -0800

    data is connecting but i dont get it why im having async problem

--- src/pages/admin/components/charts/TopicsChart.js
+++ src/pages/admin/components/charts/TopicsChart.js
@@ -4,6 +4,7 @@ import {Pie} from "react-chartjs-2"
 import Paper from "@mui/material/Paper";
 import Typography from '@mui/material/Typography';
 import jsonSrv from "../../../../Services/jsonSrv";
+import { useState } from 'react';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 //Make query on database to search for all topics and display them accorddly
@@ -14,8 +15,8 @@ const init = ()=>{
 
 	jsonSrv.get("/articles/topics.php")
 	.then((res)=>{
-		console.log(res.data);
-		let resData = res.data;
+		console.log(res.data.data.topics);
+		let resData = res.data.data.topics;
 		resData.forEach((val)=>{
 			topics.push(val.genre);
 			topicsVal.push(val.qtd);
@@ -27,36 +28,39 @@ const init = ()=>{
 		console.log(err);
 	})
 	console.log(topics);
+	let data = {
+		labels: topics,
+		datasets: [
+		  {
+			label: '# of Votes',
+			data: topicsVal,
+			backgroundColor: [
+			  'rgba(255, 99, 132, 0.2)',
+			  'rgba(54, 162, 235, 0.2)',
+			  'rgba(255, 206, 86, 0.2)',
+			  'rgba(75, 192, 192, 0.2)',
+			  'rgba(153, 102, 255, 0.2)',
+			  'rgba(255, 159, 64, 0.2)',
+			],
+			borderColor: [
+			  'rgba(255, 99, 132, 1)',
+			  'rgba(54, 162, 235, 1)',
+			  'rgba(255, 206, 86, 1)',
+			  'rgba(75, 192, 192, 1)',
+			  'rgba(153, 102, 255, 1)',
+			  'rgba(255, 159, 64, 1)',
+			],
+			borderWidth: 1,
+		  },
+		],
+	  };
 }
-init();
-export const data = {
-	labels: topics,
-	datasets: [
-	  {
-		label: '# of Votes',
-		data: topicsVal,
-		backgroundColor: [
-		  'rgba(255, 99, 132, 0.2)',
-		  'rgba(54, 162, 235, 0.2)',
-		  'rgba(255, 206, 86, 0.2)',
-		  'rgba(75, 192, 192, 0.2)',
-		  'rgba(153, 102, 255, 0.2)',
-		  'rgba(255, 159, 64, 0.2)',
-		],
-		borderColor: [
-		  'rgba(255, 99, 132, 1)',
-		  'rgba(54, 162, 235, 1)',
-		  'rgba(255, 206, 86, 1)',
-		  'rgba(75, 192, 192, 1)',
-		  'rgba(153, 102, 255, 1)',
-		  'rgba(255, 159, 64, 1)',
-		],
-		borderWidth: 1,
-	  },
-	],
-  };
+
 
 function TopicsChart() {
+	const [data, setData] = useState();
+	init();
+	
 	return (
 		<Paper elevation={3}
 		sx={{width: "50vh", display: "flex", flexDirection: "column", p: 2, alignItems: "center", textAlign: "center"}}>
