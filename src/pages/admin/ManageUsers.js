f826fab7ce1573d46109b9dc8b7038a1dd2d9583
Tyler Inari <github@al.tyler.jp>
2023-01-17 16:20:21 -0800

    resolve conflict

---
+++ src/pages/admin/ManageUsers.js
@@ -0,0 +1,17 @@
+import UserTable from "./components/tables/UserTable";
+
+import Container from "@mui/material/Container";
+import Box from '@mui/material/Box';
+
+function ManageUsers(){
+    return(
+        <>
+            <Container>
+                <Box my={2} sx={{display: "flex", flexDirection: "column"}}>
+					<UserTable />
+				</Box>
+            </Container>
+        </>
+    )
+};
+export default ManageUsers;
