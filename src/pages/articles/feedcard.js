8ffb6976c5068afa479d83126577b9eeba76e2c1
Tyler Inari <github@al.tyler.jp>
2023-01-18 12:40:24 -0800

    fix indent

--- src/pages/articles/feedcard.js
+++ src/pages/articles/feedcard.js
@@ -6,14 +6,14 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import { ThumbUp } from '@mui/icons-material';
 
-function FeedCard({post}) {
+function FeedCard({ post }) {
 
     return (
         <Grid item xs={6}>
             <Paper elevation={3}>
                 <Box padding={2}>
                     <Typography variant="h5" component="h5" marginLeft={1}>
-                        <Link to="../post_article" style={{textDecoration: 'none', color:"#538DD7"}}>
+                        <Link to="../post_article" style={{ textDecoration: 'none', color: "#538DD7" }}>
                             {post.title}
                         </Link>
                     </Typography>
@@ -34,9 +34,9 @@ function FeedCard({post}) {
                         alignItems: "center",
                         justifyContent: "space-between",
                     }}
-                >   
-                    <Typography variant="body2" component="p" marginLeft={1.5}> <ThumbUp sx={{width:18, color:"#F75959"}} /> {post.likes}</Typography>
-                    <Typography variant="body2" component="p" marginRight={2.5} sx={{fontWeight:600}}>{post.genre}</Typography>
+                >
+                    <Typography variant="body2" component="p" marginLeft={1.5}> <ThumbUp sx={{ width: 18, color: "#F75959" }} /> {post.likes}</Typography>
+                    <Typography variant="body2" component="p" marginRight={2.5} sx={{ fontWeight: 600 }}>{post.genre}</Typography>
                 </Box>
             </Paper>
         </Grid>
