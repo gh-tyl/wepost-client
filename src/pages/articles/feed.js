52cf81369fdde8d047fd1c5f2479cca59d75c1e0
junseop.shinn <jsshin0411@gmail.com>
2023-01-18 01:52:20 -0800

    Feed Page detail amend

--- src/pages/articles/feed.js
+++ src/pages/articles/feed.js
@@ -5,7 +5,6 @@ import Grid from '@mui/material/Grid';
 import jsonSrv from '../../Services/jsonSrv';
 import { useState } from 'react';
 import { useEffect } from 'react';
-import { Typography } from '@mui/material';
 
 function Feed() {
 	const [res, setRes] = useState([]);
@@ -25,22 +24,6 @@ function Feed() {
 
 	return (
 		<div>
-			{/* <table border={1}>
-				<thead>
-					<tr>
-						<th>Title</th>
-						<th>Name</th>
-					</tr>
-				</thead>
-				<tbody>
-					{res.map((post,key)=>
-						<tr key={key}>
-							<td>{post.title}</td>
-							<td>{post.last_name}</td>
-						</tr>
-					)}
-				</tbody>
-			</table> */}
 			<Container sx={{ marginY: 3 }}>
 				<Grid container spacing={3}>
 				{res.map((post,key)=> <FeedCard post={post} key={key}/>)}
--- src/pages/articles/feedcard.js
+++ src/pages/articles/feedcard.js
@@ -1,9 +1,10 @@
 // This is the feedcard page
+import { Link } from "react-router-dom";
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import { color } from '@mui/system';
+import { ThumbUp } from '@mui/icons-material';
 
 function FeedCard({post}) {
 
@@ -12,7 +13,9 @@ function FeedCard({post}) {
             <Paper elevation={3}>
                 <Box padding={2}>
                     <Typography variant="h5" component="h5" marginLeft={1}>
-                        {post.title}
+                        <Link to="../post_article" style={{textDecoration: 'none', color:"#538DD7"}}>
+                            {post.title}
+                        </Link>
                     </Typography>
                 </Box>
                 <Box paddingLeft={2} paddingBottom={1}
@@ -31,9 +34,9 @@ function FeedCard({post}) {
                         alignItems: "center",
                         justifyContent: "space-between",
                     }}
-                >
-                    <Typography variant="body2" component="p" marginLeft={1.5}>{post.likes}</Typography>
-                    <Typography variant="body2" component="p" marginRight={2.5}>{post.genre}</Typography>
+                >   
+                    <Typography variant="body2" component="p" marginLeft={1.5}> <ThumbUp sx={{width:18, color:"#F75959"}} /> {post.likes}</Typography>
+                    <Typography variant="body2" component="p" marginRight={2.5} sx={{fontWeight:600}}>{post.genre}</Typography>
                 </Box>
             </Paper>
         </Grid>
