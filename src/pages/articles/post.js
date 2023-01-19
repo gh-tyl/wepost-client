98a902d61e6c468193398c71336065b1042de34f
Angel_Laptop <aalopez15.11@gmail.com>
2023-01-18 20:10:35 -0800

    edit function added

--- src/pages/articles/post.js
+++ src/pages/articles/post.js
@@ -8,6 +8,7 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import { ThumbUp } from '@mui/icons-material';
+import DeleteIcon from '@mui/icons-material/Delete';
 function PostArticle() {
 	const [queryStr] = useSearchParams();
 	const [postData,setData] = useState("");
@@ -20,7 +21,7 @@ function PostArticle() {
 			id = queryStr.get("id")
 		}
 		if(queryStr.get("edit") != null){
-			setEditFlag(queryStr.get("edit"))
+			if(queryStr.get("edit") == "true")setEditFlag(true)
 		}
 		formData.append('post_id',id)
 		jsonSrv.post("/pages/articles/post_article.php",formData)
@@ -29,11 +30,38 @@ function PostArticle() {
 			setData(res.data);
 		})
 	}
-	const editArticle = ()=>{
-
+	const editUrl = ()=>{
 		navigate(`/post_article?id=${postData.post.id}&&edit=true`);
 		setEditFlag(true);
-		console.log(editFlag);
+	}
+	const cancelEdit = ()=>{
+		navigate(`/post_article?id=${postData.post.id}`);
+		setEditFlag(false);
+
+	}
+	const calcHeight=(value)=> {
+		let numberOfLineBreaks = (value.match(/\n/g) || []).length;
+		let newHeight = 20 + numberOfLineBreaks * 20 + 12 + 2;
+		return newHeight;
+	  }
+	const textAreaHeight = ()=>{
+		let txtarea = document.querySelector('#txtarea');
+		txtarea.style.height = (calcHeight(txtarea.value)+20) + "px";
+		console.log(calcHeight(txtarea.value) + "px");
+		return null;
+	}
+	const editArticle = (e)=>{
+		e.preventDefault();
+		let formData = new FormData(e.target);
+		formData.append('path',postData.post.content_path);
+		console.log(formData);
+		jsonSrv.post("/pages/articles/edit_post.php",formData)
+		.then(res=>{
+			console.log(res.data);
+			
+			loadArticle();
+			cancelEdit();
+		})
 	}
 	useEffect(()=>{loadArticle()},[])
 	return (
@@ -45,23 +73,34 @@ function PostArticle() {
 			(postData != "")?
 				< >
 					<div style={{justifyContent: 'center' ,display: 'flex'}} key={postData.post.id}>
-						<Card sx={{ minWidth: 275 ,width:'80%'} }>
+						<Card  sx={{ minWidth: 275 ,width:'80%', padding : 3} } >
 								<CardContent >
 									<Typography variant='h5' component='div'>
 										{postData.post.title}
 									</Typography>
 									<Typography sx={{ fontSize: 14 }} color='text.secondary' gutterBottom>
-									
 									Posted by: {postData.post.first_name} {postData.post.last_name}
 									</Typography>
 									<Typography sx={{ fontSize: 14 }} color='text.secondary' gutterBottom>
 									Date posted: {(postData.post.datetime)}
 									</Typography>
+									<Typography sx={{ fontSize: 14 }} color='text.secondary' gutterBottom>
+									Genre: {(postData.post.genre)}
+									</Typography>
 									<Typography sx={{ mb: 1.5 }} color='text.secondary'>
 									<ThumbUp sx={{width:18, color:"#F75959"}} />{postData.post.likes}
 									</Typography>
+									<Typography variant='body2' sx={{textAlign: "justify"}}>
+									 {postData.content}
+									</Typography>
+									<CardActions>
+									<Button onClick={editUrl} variant="outlined" type="button" size='small'>Edit</Button>
+								</CardActions>
 									<Typography variant='body2'>
-									 "content"
+									 <br/>
+									</Typography>
+									<Typography variant='h5' component='div'>
+										Comments:
 									</Typography>
 									<Typography variant='body2'>
 									 <br/>
@@ -70,7 +109,7 @@ function PostArticle() {
 									{postData.comments.map((comment,idx) =>
 
 										<>
-										<Card sx={{ width: "80%" ,backgroundColor: 'lightgray',padding: "1vh	"}} key={idx} >
+										<Card sx={{backgroundColor: 'lightgray',padding: "1vh	"}} key={idx} >
 											<Typography sx={{ fontSize: 14 }} color='text.secondary' gutterBottom>
 												{comment.comment}
 											</Typography>
@@ -85,9 +124,7 @@ function PostArticle() {
 									)}
 									</div>
 								</CardContent>
-								<CardActions>
-									<Button onClick={editArticle} type="button" size='small'>Edit</Button>
-								</CardActions>
+								
 						</Card>
 					</div>
 					<br></br>
@@ -97,8 +134,8 @@ function PostArticle() {
 				:(postData != "")?
 				< >
 					<div style={{justifyContent: 'center' ,display: 'flex'}} key={postData.post.id}>
-						<Card sx={{ minWidth: 275 ,width:'80%'} }>
-								<form >
+						<Card sx={{ minWidth: 275 ,width:'80%', padding : 3} }>
+								<form onSubmit={editArticle}>
 								<CardContent >
 									<Typography variant='h5' component='div'>
 										{postData.post.title}
@@ -110,11 +147,24 @@ function PostArticle() {
 									<Typography sx={{ fontSize: 14 }} color='text.secondary' gutterBottom>
 									Date posted: {(postData.post.datetime)}
 									</Typography>
+									<Typography sx={{ fontSize: 14 }} color='text.secondary' gutterBottom>
+									Genre: {(postData.post.genre)}
+									</Typography>
 									<Typography sx={{ mb: 1.5 }} color='text.secondary'>
 									<ThumbUp sx={{width:18, color:"#F75959"}} />{postData.post.likes}
 									</Typography>
+									<textarea name='newContent' style={{textAlign: "justify", width: "99%",height: "300px",resize:"none"}} id="txtarea" onChange={textAreaHeight} onFocus={textAreaHeight} defaultValue={postData.content}  >
+										
+									</textarea>
+									<CardActions>
+										<Button onClick={cancelEdit} variant="outlined" type="button" size='small' color='error'><DeleteIcon fontSize="small" />Cancel</Button>
+										<Button  variant="outlined" type="submit" size='small' color='success'>Save</Button>
+									</CardActions>
 									<Typography variant='body2'>
-									 "content"
+									 <br/>
+									</Typography>
+									<Typography variant='h5' component='div'>
+										Comments:
 									</Typography>
 									<Typography variant='body2'>
 									 <br/>
@@ -123,7 +173,7 @@ function PostArticle() {
 									{postData.comments.map((comment,idx) =>
 
 										<>
-										<Card sx={{ width: "80%" ,backgroundColor: 'lightgray',padding: "1vh	"}} key={idx} >
+										<Card sx={{backgroundColor: 'lightgray',padding: "1vh"}} key={idx} >
 											<Typography sx={{ fontSize: 14 }} color='text.secondary' gutterBottom>
 												{comment.comment}
 											</Typography>
@@ -138,10 +188,6 @@ function PostArticle() {
 									)}
 									</div>
 								</CardContent>
-								<CardActions>
-									<Button onClick={editArticle} type="button" size='small' color='error'>Cancel</Button>
-									<Button onClick={editArticle} type="button" size='small' color='success'>Save</Button>
-								</CardActions>
 							</form>
 						</Card>
 					</div>
