66625e8c097e9646cd415ce331298b3a5381648d
mikufujiwara <llt9.oct22@gmail.com>
2023-01-19 15:10:00 -0800

    home design

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,29 @@
 // This is the Home page
 // It's going to be the welcome page
 // Write the introduction here
+import { Container, Typography} from "@mui/material";
 
 function Home() {
 	return (
 		<>
-			<h1>What is WePost?</h1>
+			<Container maxWidth="sm">
+				<div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+					<Typography variant="h2" component="h2" sx={{ fontWeight: "bold", color: "#082c69" }} >
+						WePost
+					</Typography>
+					<Typography variant="h6" component="h2" sx={{ backgroundColor: "#f0f4fa", p: "20px", mt: "20px", color: "#1c1c1c" }}>
+						<Typography variant="h5" component="h2" sx={{ fontWeight: "bold", color: "#082c69" }} >
+							What is WePost?
+						</Typography>
+						WePost is an encyclopedia where you can post articles from simple ideas up to a full scientific article!
+
+						<Typography variant="h5" component="h2" sx={{ fontWeight: "bold", color: "#082c69", mt: "10px" }} >
+							Our source is Reliable!
+						</Typography>
+						We make sure scientific articles come from a trustable source, it requires a scientific ID from a valid institution.
+					</Typography>
+				</div>
+			</Container>
 		</>
 	)
 }
